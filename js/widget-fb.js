7ee582ad7a2c3137f0b59f4eaa791191998a0a28
jsguachinche <36742009+jsguachinche@users.noreply.github.com>
2018-03-01 09:53:55 +0000

    Update widget-fb.js

--- js/widget-fb.js
+++ js/widget-fb.js
@@ -23,3 +23,16 @@ js = d.createElement(s); js.id = id;
 js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.12&appId=568070633526032&autoLogAppEvents=1';
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+function loginFacebook() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function (response) {
+                console.log('Good to see you, ' + response.name + '.');
+            });
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    });
+}
