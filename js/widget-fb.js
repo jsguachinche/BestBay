dbf6694ab9a481c7c9a54e9c792a8d23ff381bbf
jsguachinche <36742009+jsguachinche@users.noreply.github.com>
2018-03-01 09:59:53 +0000

    Update widget-fb.js

--- js/widget-fb.js
+++ js/widget-fb.js
@@ -10,10 +10,23 @@ window.fbAsyncInit = function () {
     FB.getLoginStatus(function (response) {
         statusChangeCallback(response);
         console.log(response);
+        
     });
     FB.logout(function(response) {
 // Person is now logged out
     });
+    function loginFacebook() {
+        FB.login(function (response) {
+            if (response.authResponse) {
+                console.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function (response) {
+                    console.log('Good to see you, ' + response.name + '.');
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        });
+    }
     
 };
 (function(d, s, id) {
@@ -24,15 +37,3 @@ js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.12&appId=
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-function loginFacebook() {
-    FB.login(function (response) {
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function (response) {
-                console.log('Good to see you, ' + response.name + '.');
-            });
-        } else {
-            console.log('User cancelled login or did not fully authorize.');
-        }
-    });
-}
