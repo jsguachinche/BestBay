2cef727c18ec18428cad0c27917fbaa5fb3997eb
aschausson <aschausson@gmail.com>
2018-03-04 07:56:43 +0000

    Add files via upload
    Código refactorizado y comentado. Múltiples arreglos de bugs (filtrado, control de errores..)

--- js/app.js
+++ js/app.js
@@ -1,37 +1,69 @@
+
 var apikey = 'jsguachi-JSGuachi-PRD-843252cae-6ff77db1'; // ebay
+var apikeyBB = 'A0iJvovzx1h8jN9IXhGSCwjm'; //bestbuy
 var apikeyForex = 'BunYSIcXTAp38eCsdvLVzkflhfM7VPxH'; // forex
 var value;
 var ultimoTipoBusqueda;
+var buscandoEB = false;
+var buscandoBB = false;
+var productosEB = [];
+
+/**
+ * Tipo Producto, representa un producto de Ebay o BestBay, con las características que queremos dar al usuario.
+ * @param {*} orden 
+ * @param {*} thumb 
+ * @param {*} url 
+ * @param {*} nombre 
+ * @param {*} precio 
+ * @param {*} moneda 
+ */
+function Producto(orden, thumb, url, nombre, precio, moneda) {
+  this.orden = orden;
+  this.thumb = thumb;
+  this.url = url;
+  this.nombre = nombre;
+  this.precio = precio;
+  this.moneda = moneda;
+  if (moneda == 'EUR')
+    this.moneda = '€';
+}
+
+/**
+ * Cuando el documento está preparado, se crea la URL de la llamada Ebay y se asignan los listener de los botones.
+ */
+$(document).ready(function () {
+  // Execute the function to build the URL filter
+  buildURLArray(filterarray);
+  $('.productos').find('button').click(function () {
+    filtrar(this);
+  });
+});
+
+/**
+ * Se hace una lista de objetos producto, se crean con react y se paginan. Este método es un callback de la llamada a la API, y recibe los resultados de la llamada.
+ * @param {*} root 
+ */
 function _cb_findItemsByKeywords(root) {
   var items = root.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-  var html = [];
-  html.push('<table class="table table-hover "><tbody>');
+  productosEB = [];
+
+  $('.tableEB tbody').empty();
   for (var i = 0; i < items.length; ++i) {
     var item = items[i];
-    var title = item.title;
-    var pic = item.galleryURL;
-    var viewitem = item.viewItemURL;
-    var price = item.sellingStatus[0].currentPrice[0].__value__;
-    var type = item.sellingStatus[0].currentPrice[0]["@currencyId"];
-    if (null != title && null != viewitem) {
-      html.push(`<tr class="tbSelector"><th score="row">${i + 1}<td><img src="${pic}" class="img-thumbnail"></td>
-          <td><a href="${viewitem}" title="${title}" target="_blank">${title}</a></td><td>Precio: ${price} tipo moneda: ${type}</td></tr>`);
-    }
+    var productoEB = new Producto(i + 1, item.galleryURL, item.viewItemURL, item.title, item.sellingStatus[0].currentPrice[0].__value__, item.sellingStatus[0].currentPrice[0]["@currencyId"]);
+    productosEB.push(productoEB);
   }
-  html.push('</tbody></table>');
-  document
-    .getElementById("ContItem")
-    .innerHTML = html.join("");
+
+  productListReact(productosEB, '#contItem', '.tableEB tbody');
   $('.ContItem').append('<div id="pagination-1"></div>');
 
-  /*paginado*/
-  /*
   paginate({
-    itemSelector: '.tbSelector',
+    itemSelector: '.eb',
     paginationSelector: '#pagination-1',
     itemsPerPage: 10
-  });*/
+  });
 }
+
 var filterarray = [
   {
     "name": `MaxPrice`,
@@ -60,16 +92,17 @@ var filterarray = [
 
 var urlfilter = "";
 
+/** 
+ * Función que construye los filstros de la URL.
+*/
 function buildURLArray() {
   // Iterate through each filter in the array
   for (var i = 0; i < filterarray.length; i++) {
-    //Index each item filter in filterarray
     var itemfilter = filterarray[i];
-    // Iterate through each parameter in each item filter
     for (var index in itemfilter) {
-      // Check to see if the paramter has a value (some don't)
+      // Para los filtros que tengan valor
       if (itemfilter[index] !== "") {
-        if (itemfilter[index]instanceof Array) {
+        if (itemfilter[index] instanceof Array) {
           for (var r = 0; r < itemfilter[index].length; r++) {
             var value = itemfilter[index][r];
             urlfilter += "&itemFilter\(" + i + "\)." + index + "\(" + r + "\)=" + value;
@@ -80,98 +113,28 @@ function buildURLArray() {
       }
     }
   }
-} // End buildURLArray() function
+}
 
-// Execute the function to build the URL filter
-buildURLArray(filterarray);
-
-$('.productos')
-  .find('button')
-  .click(function () {
-    // value = $(this).val(); console.log(value); valueBuscar =
-    // $('#searchvalue').val(); var filterPrice = $('#filter-price').val(); //
-    // Construct the request // Replace MyAppID with your Production AppID var url =
-    // `http://svcs.ebay.com/services/search/FindingService/v1`; url +=
-    // `?OPERATION-NAME=findItemsByKeywords`; url += `&SERVICE-VERSION=1.0.0`; url
-    // += `&SECURITY-APPNAME=${apikey}`; url += `&GLOBAL-ID=EBAY-ES`; url +=
-    // `&RESPONSE-DATA-FORMAT=JSON`; url += `&callback=_cb_findItemsByKeywords`; url
-    // += `&REST-PAYLOAD`; url += `&keywords=${valueBuscar}`; url +=
-    // `&paginationInput.entriesPerPage=200`; url += urlfilter; url +=
-    // `&sortOrder=BestMatch` // Submit the request s =
-    // document.createElement('script'); // create script element s.src = url;
-    // document.body.appendChild(s); // $('#filter-price').show('slow');
-    // peticionAJAX(value);
-
-    $('#title').hide();
-
-    valueType = $(this).val();
-
-    ultimoTipoBusqueda = valueType;
-
-    var filterPrice = $('#filter-price').val();
-    value = $('#searchvalue').val() + " " + valueType;
-
-    if (value == "") {
-      value = $(this).val();
-    }
-    // Construct the request Replace MyAppID with your Production AppID
-    $('#results').empty();
-    var filterPrice = $('#filter-price').val();
-    var url = `http://svcs.ebay.com/services/search/FindingService/v1`;
-    url += `?OPERATION-NAME=findItemsByKeywords`;
-    url += `&SERVICE-VERSION=1.0.0`;
-    url += `&SECURITY-APPNAME=${apikey}`;
-    url += `&GLOBAL-ID=EBAY-ES`;
-    url += `&RESPONSE-DATA-FORMAT=JSON`;
-    url += `&callback=_cb_findItemsByKeywords`;
-    url += `&REST-PAYLOAD`;
-    url += `&keywords=${value}`;
-    url += `&paginationInput.entriesPerPage=200`;
-    url += `&sortOrder=${filterPrice}`
-
-    // // Submit the request s = document.createElement('script'); // create script
-    // element s.src = url; document.body.appendChild(s); peticionAJAX(value);
-
-    $.ajax({
-      url: url,
-      jsonp: "callback",
-      dataType: "jsonp",
-      beforeSend: function () {
-        $('.loader').addClass('is-active');
-      },
-      complete: function (element) {
-        $('.loader').removeClass('is-active');
-        // console.warn(element);
-      },
-      error: function (error, codigo, desc) {
-        // alert(`Error: ${error} Codigo: ${codigo} Descripcion: ${desc}`);
-      }
-    });
-
-  });
-
-function peticionAJAX(e) {
-  let v = e;
-  let url = `https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=${apikey}&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${v}&paginationInput.entriesPerPage=20&GLOBAL-ID=EBAY-ES&siteid=0`
-  $.ajax({
-    url: url,
-    jsonp: "callback",
-    dataType: "jsonp",
-    beforeSend: function () {
-      $('.loader').addClass('is-active');
-    },
-    complete: function (element) {
-      $('.loader').removeClass('is-active');
-      console.warn(element);
-    },
-    error: function (error, codigo, desc) {
-      // alert(`Error: ${error} Codigo: ${codigo} Descripcion: ${desc}`);
+/** 
+ * Hace que solo sea posible seleccionar un checkbox de cada grupo.
+*/
+function unCheckbox(){
+  $("input:checkbox").on('click', function() {
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
     }
   });
 }
 
-function Login() {}
-
+/**
+ * Se paginan los resultados de la búsqueda.
+ * @param {1} options 
+ */
 function paginate(options) {
   var items = $(options.itemSelector);
   var numItems = items.length;
@@ -182,7 +145,7 @@ function paginate(options) {
   $(options.paginationSelector).pagination({
     items: numItems,
     itemsOnPage: perPage,
-    cssStyle: "dark-theme",
+    cssStyle: "compact-theme",
     onPageClick: function (pageNumber) {
       var showFrom = perPage * (pageNumber - 1);
       var showTo = showFrom + perPage;
@@ -195,68 +158,85 @@ function paginate(options) {
   });
 }
 
-function filtrar() {
-  ebayFiltrado();
-  bestBuyFiltado();
+/**
+ * Se llama a los métodos de busqueda de las dos APIs.
+ * @param {*} tipo 
+ */
+function filtrar(tipo) {
+
+  valueType = $(tipo).val();
+  if (tipo != undefined)
+    ultimoTipoBusqueda = valueType;
+
+  value = filtrarBusqueda();
+  bestBuyFiltrado();
+  value = ultimoTipoBusqueda + value;
+  ebayFiltrado(tipo);
+
+  unCheckbox()
 }
 
+/** 
+ * Se crean los filtros para la URL según las opciones elegidas.
+*/
 function filtrarBusqueda() {
 
   var filtroFinal = "";
 
-  if (ultimoTipoBusqueda == "Smartphone") {
+  var search = $('#searchvalue').val();
+  var arraySearch = []
+  arraySearch = search.split(' ');
+    for (let i = arraySearch.length - 1; i >= 0; i--) {
+      if (arraySearch[i] != '')
+      filtroFinal += ('%20' + arraySearch[i]);
+    }
+
+  if (ultimoTipoBusqueda == "smartphone") {
     // var marca = [],   memoria = [],   ram = [],   conectividad = [],   color =
     // [],   os = []; Marca
     var arrayMarcaQuery = document.querySelectorAll("input[name='movilMarca']:checked");
-
     arrayMarcaQuery.forEach(e => {
       // marca.push(e.value);
-      filtroFinal += e.value + " ";
+      filtroFinal += '%20' + e.value;
     });
 
     // Memoria
     var arrayMemoriaQuery = document.querySelectorAll("input[name='movilMemoria']:checked");
-
     arrayMemoriaQuery.forEach(e => {
       // memoria.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // Ram
     var arrayRamQuery = document.querySelectorAll("input[name='movilRam']:checked");
-
     arrayRamQuery.forEach(e => {
       // ram.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // Conectividad
     var arrayConectividadQuery = document.querySelectorAll("input[name='movilConectividad']:checked");
-
     arrayConectividadQuery.forEach(e => {
       // conectividad.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // Color
     var arrayColorQuery = document.querySelectorAll("input[name='movilColor']:checked");
-
     arrayColorQuery.forEach(e => {
       // color.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // OS
     var arrayOSQuery = document.querySelectorAll("input[name='movilOS']:checked");
-
     arrayOSQuery.forEach(e => {
       // os.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
-
   }
-  if (ultimoTipoBusqueda == "TV") {
+
+  if (ultimoTipoBusqueda == "tv") {
     var marca = [],
       pantalla = [],
       color = [],
@@ -265,89 +245,75 @@ function filtrarBusqueda() {
 
     // Marca
     var arrayMarcaQuery = document.querySelectorAll("input[name='tvMarca']:checked");
-
     arrayMarcaQuery.forEach(e => {
       // marca.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
 
     // pantalla
     var arrayPantallaQuery = document.querySelectorAll("input[name='tvPantalla']:checked");
-
     arrayPantallaQuery.forEach(e => {
       // pantalla.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // color
     var arrayColorQuery = document.querySelectorAll("input[name='tvColor']:checked");
-
     arrayColorQuery.forEach(e => {
       // color.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // pulgadas
     var arrayPulgadasQuery = document.querySelectorAll("input[name='tvPulgadas']:checked");
-
     arrayPulgadasQuery.forEach(e => {
       // pulgadas.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
+
     // resolucion
     var arrayResolucionQuery = document.querySelectorAll("input[name='tvResolucion']:checked");
-
     arrayResolucionQuery.forEach(e => {
       // resolucion.push(e.value);
-      filtroFinal += e.value + " ";
-
-    });
-
-  } else {
-
-    // var tipo = [] Marca
-    var arrayTipoQuery = document.querySelectorAll("input[name='fitnessTipo']:checked");
-
-    arrayTipoQuery.forEach(e => {
-      // tipo.push(e.value);
-      filtroFinal += e.value + " ";
-
+      filtroFinal += '%20' + e.value;
     });
   }
-
+  else {
+    // var tipo = [] Marca
+    var arrayTipoQuery = document.querySelectorAll("input[name='fitnessTipo']:checked");
+    arrayTipoQuery.forEach(e => {
+      // tipo.push(e.value);
+      filtroFinal += '%20' + e.value;
+    });
+  }
   return filtroFinal;
-
 }
 
-function bestBuyFiltado() {
-  value = encodeURI($('#searchvalue').val() + " " + filtrarBusqueda());
-
-  if (ultimoTipoBusqueda == "Smartphone") {
+/** 
+ * Se llama a la búsqueda de la API BestBuy, dependiendo de la categoría.
+*/
+function bestBuyFiltrado() {
+  
+  if (ultimoTipoBusqueda == "smartphone") {
     readBBJson(0, value);
   }
 
-  if (ultimoTipoBusqueda == "TV") {
+  if (ultimoTipoBusqueda == "tv") {
     readBBJson(1, value);
   }
-  if (ultimoTipoBusqueda == "Health") {
+  if (ultimoTipoBusqueda == "fitness") {
     readBBJson(2, value);
   }
 }
 
-function ebayFiltrado() {
-  // $('#title').hide();
-
-  value = encodeURI($('#searchvalue').val() + " " + filtrarBusqueda());
-
+/**
+ * Se crea la URL y se aplical los filtros para EBay.
+ * @param {*} tipo 
+ */
+function ebayFiltrado(tipo) {
+  $('#title').hide();
   var filterPrice = $('#filter-price').val();
-  // value = $('#searchvalue').val() + " " + valueType;
 
-  if (value == "") {
-    value = $(this).val();
-  }
-  // Construct the request Replace MyAppID with your Production AppID
   $('#results').empty();
   var filterPrice = $('#filter-price').val();
   var url = `http://svcs.ebay.com/services/search/FindingService/v1`;
@@ -361,24 +327,23 @@ function ebayFiltrado() {
   url += `&keywords=${value}`;
   url += `&paginationInput.entriesPerPage=200`;
   url += `&sortOrder=${filterPrice}`
-  
-  // // Submit the request s = document.createElement('script'); // create script
-  // element s.src = url; document.body.appendChild(s); peticionAJAX(value);
 
   $.ajax({
     url: url,
     jsonp: "callback",
     dataType: "jsonp",
     beforeSend: function () {
+      buscandoEB = true;
       $('.loader').addClass('is-active');
     },
     complete: function (element) {
-      $('.loader').removeClass('is-active');
-      // console.warn(element);
+      buscandoEB = false;
+      if ((buscandoEB == false) && (buscandoBB == false))
+        $('.loader').removeClass('is-active');
     },
-    error: function (error, codigo, desc) {
-      // alert(`Error: ${error} Codigo: ${codigo} Descripcion: ${desc}`);
+    fail: function (error, codigo, desc) {
+      toastr.error('', 'Error conectando con Ebay')
+      buscandoEB = false;
     }
   });
-
 }
--- js/reactproducto.js
+++ js/reactproducto.js
@@ -1,33 +1,74 @@
 
 
 
-  var ProductoComponent = React.createClass({
-    componentDidMount: function() {
-      console.log('component did mount - stuff to clean up later');
-    },
-    render: function(props) {
-      return (
-                <tr className="tbSelector">
-                    <th score="row">{this.props.producto.orden}</th>
-                    <td>
-                        <img src={this.props.producto.thumb} className="img-thumbnail"/>
-                    </td>
-                    <td>
-                        <a href={this.props.producto.url} title={this.props.producto.nombre} target="_blank">{this.props.producto.nombre}</a>
-                    </td>
-                    <td>Precio: {this.props.producto.precio}€</td>
-                </tr>
-      );
-    },
-    componentWillUnmount: function() {
-      console.log('about to unmount - clean up stuff here');
-    }
-  });
-  
-  function productoReact(producto, element) {
-    ReactDOM.render(
-      <ProductoComponent producto={producto} />,
-      element
+var ProductoBBComponent = React.createClass({
+  componentDidMount: function () {
+    console.log('component did mount - stuff to clean up later');
+  },
+  render: function (props) {
+    return (
+      <tr className="tbSelector bb">
+        <th score="row">{this.props.producto.orden}</th>
+        <td>
+          <img src={this.props.producto.thumb} className="img-thumbnail" />
+        </td>
+        <td>
+          <a href={this.props.producto.url} title={this.props.producto.nombre} target="_blank">{this.props.producto.nombre}</a>
+        </td>
+        <td>Precio: {this.props.producto.precio}€</td>
+      </tr>
     );
+  },
+  componentWillUnmount: function () {
+    console.log('about to unmount - clean up stuff here');
   }
+});
+
+/**
+ * Renderiza el componente react de un producto BestBay.
+ * @param {*} producto 
+ * @param {*} element 
+ */
+function productoBBReact(producto, element) {
+  ReactDOM.render(
+    <ProductoBBComponent producto={producto} />,
+    element
+  );
+}
+
+
+var ProductoEBComponent = React.createClass({
+  componentDidMount: function () {
+    console.log('component did mount - stuff to clean up later');
+  },
+  render: function (props) {
+    return (
+      <tr className="tbSelector eb">
+        <th score="row">{this.props.producto.orden}</th>
+        <td>
+          <img src={this.props.producto.thumb} className="img-thumbnail" />
+        </td>
+        <td>
+          <a href={this.props.producto.url} title={this.props.producto.nombre} target="_blank">{this.props.producto.nombre}</a>
+        </td>
+        <td>Precio: {this.props.producto.precio}{this.props.producto.moneda}</td>
+      </tr>
+    );
+  },
+  componentWillUnmount: function () {
+    console.log('about to unmount - clean up stuff here');
+  }
+});
+
+/**
+ * Renderiza el componente react de un producto Ebay.
+ * @param {*} producto 
+ * @param {*} element 
+ */
+function productoEBReact(producto, element) {
+  ReactDOM.render(
+    <ProductoEBComponent producto={producto} />,
+    element
+  );
+}
 
